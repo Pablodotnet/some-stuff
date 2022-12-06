9315f7b1678f48e477633ee22aeadceb2232ab3d
Pablo <luis.pablo.lopez.lopez@uabc.edu.mx>
2022-12-05 20:49:40 -0800

    Add UI

---
+++ apps/manager/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import Dashboard from './dashboard';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="manager" />
+      <div />
+    </>
+  );
+}
+
+export default App;
